b1edab6f625bb7f270bda96a6f891921befb8f7f
Vincent-Carrier <vincentcarrier15@gmail.com>
2019-11-14 12:08:50 -0500

    Fix y-axis, extract into small funcs, add support for options

--- index.js
+++ index.js
@@ -1,50 +1,63 @@
 import $ from 'jquery'
+import './style.scss'
 
 function maxOf(arr) {
-  return arr.slice().sort((a, b) =>  b.value - a.value)[0].value
+  return arr.slice().sort((a, b) => b.value - a.value)[0].value
 }
 
-function drawBarChart(data, options, element) {
-  if ($.isNumeric(data[0])) {
-    data = data.map(e => ({value: e, label: ''}))
-  }
+function buildYAxis(max) {
+  const $yAxis = $('<div class="y-axis"/>')
+  const step = max / 5
+  const yTicks = [5, 4, 3, 2, 1, 0].map(n => n * step)
+  const $yTicks = yTicks.map(n => $(`<div>${n} -</div>`))
+  return $yAxis.html($yTicks)
+}
 
-  const $chartContainer = $(element).addClass('chart-container')
+function buildXAxis(data) {
+  const $xAxis = $('<div class="x-axis"/>')
+  const $xTicks = data.map(e => $(`<div>${e.label}</div>`))
+  return $xAxis.html($xTicks)
+}
 
-  const max = maxOf(data)
+function buildGraph(data, max) {
   const normalized = data.map(e => e.value / max)
   const $chart = $('<div class="chart"/>')
   const $bars = normalized.map(n =>
-    $('<div class="bar"/>').height(`${n * 90}%`)
+    $('<div class="bar"/>').height(`${n * 95}%`)
   )
-  $chart.html($bars)
+  return $chart.html($bars)
+}
 
-  const $yAxis = $('<div class="y-axis"/>')
-  const step = max / 4
-  const yTicks = [4,3,2,1].map((n) => n * step)
-  const $yTicks = yTicks.map(n => $(`<div>${n} -</div>`))
-  $yAxis.html($yTicks)
+export function drawBarChart(data, options, element) {
+  if ($.isNumeric(data[0])) {
+    data = data.map(e => ({ value: e, label: '' }))
+  }
+  const maxY = maxOf(data)
 
-  const $xAxis = $('<div class="x-axis"/>')
-  const $xTicks = data.map(e => $(`<div>${e.label}</div>`))
-  $xAxis.html($xTicks)
+  const $chartContainer = $(element).addClass('chart-container')
+                                    .height(options.height)
+                                    .width(options.width)
+  const $title = $(`<h1 class="chart-title">${options.title}</h1>`)
+  const $graph = buildGraph(data, maxY)
+  const $yAxis = buildYAxis(maxY)
+  const $xAxis = buildXAxis(data)
 
-  const $title = $('<h1 class="title">My Title</h1>')
-
-  $chartContainer.append($title, $chart, $yAxis, $xAxis)
+  $chartContainer.append($title, $graph, $yAxis, $xAxis)
 }
 
 const data = [
-  {value: 25, label: 'first'},
-  {value: 18, label: 'second'},
-  {value: 20, label: 'third'},
-  {value: 12, label: 'fourth'},
-  {value: 22, label: 'fifth'},
-  {value: 6, label: 'sixth'},
+  { value: 25, label: 'first' },
+  { value: 18, label: 'second' },
+  { value: 20, label: 'third' },
+  { value: 12, label: 'fourth' },
+  { value: 22, label: 'fifth' },
+  { value: 6, label: 'sixth' },
 ]
 
 const options = {
-
+  width: '500px',
+  height: '400px',
+  title: 'A great title'
 }
 
 $(document).ready(() =>
